f6360e7c0c4dfa9fb64dde944a248bd3e1c26feb
Hema Parkhande <hemag.gade@gmail.com>
2024-12-10 13:18:59 +0100

    second commit
    second

---
+++ AssertKeyword.java
@@ -0,0 +1,19 @@
+package Practice_Math_Class;
+
+public class AssertKeyword 
+{
+
+	public static void main(String[] args) 
+	{		
+		int age =10;
+		assert age>18: "Sorry your age is not 18 or more than 18";
+		System.out.println("You can vote");
+		
+	}
+	
+
+}
+
+// 2 ways to write assert condition; and assert condition:"Message";
+//throw exception
+//
